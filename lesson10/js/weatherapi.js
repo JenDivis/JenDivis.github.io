0531e73daa90d8067c6baf527ef8dc2298ea2416
JenDivis <jennycarnline@gmail.com>
2021-03-14 01:23:01 -0500

    weather api preston so far

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -8,7 +8,7 @@ fetch(apiURL)
         //print results to console
         console.log(jsObject);
         //write temp value to current-temp id
-        //document.getElementById("current-temp").textContent = jsObject.main.temp;
+        document.getElementById("current-temp").textContent = jsObject.main.temp;
         
         //variable to store the image address which needs to be concatenated together given the API icon code value result.
         const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
@@ -17,12 +17,12 @@ fetch(apiURL)
         //Assign the <span> tag  with the id of 'imagesrc' the concatenated image variable to to display the full address for testing purposes.
         //document.getElementById("imagesrc").textContent = imagesrc;
         //Set the src attribute for the <img> with the id of 'icon'.
-        document.getElementById("icon").setAttribute('src', imagesrc);
-        document.getElementById("icon").setAttribute('alt', desc);
+        document.getElementById("weatherIcon").setAttribute('src', imagesrc);
+        document.getElementById("weatherIcon").setAttribute('alt', desc);
 
-        //Preston page
+        //Weather Summary
         document.getElementById("current").textContent = jsObject.weather[0].description;
-        document.getElementById("temp").textContent = jsObject.main.temp;
+        document.getElementById("temp").textContent = Math.round(jsObject.main.temp);
         document.getElementById("humidity").textContent = jsObject.main.humidity;
-        document.getElementById("windSpeed").textContent = jsObject.wind.speed;
+        document.getElementById("windSpeed").textContent = Math.round(jsObject.wind.speed);
     });
